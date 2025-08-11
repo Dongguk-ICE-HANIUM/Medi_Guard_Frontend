564de195a6f7be67ee160ca74bec71c53c1e130b
iseun15 <ave0315@naver.com>
2025-08-11 11:48:13 +0900

    [feat] group.tsx ui 구현 완료

--- components/register/group/Group.tsx
+++ components/register/group/Group.tsx
@@ -1,14 +1,56 @@
-import FontAwesome from "@expo/vector-icons/FontAwesome";
-import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { colors } from "@/constants";
+import React, { useState } from "react";
+import {
+  StyleSheet,
+  Switch,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from "react-native";
 
 const Group = () => {
+  const [isGroupEnabled, setIsGroupEnabled] = useState(false);
+  const [searchText, setSearchText] = useState("");
+
+  const handleGroupToggle = (value: boolean) => {
+    setIsGroupEnabled(value);
+    if (!value) {
+      setSearchText("");
+    }
+  };
+  const handleSearchPress = () => {
+    console.log("찾아보기 버튼 클릭");
+  };
+
   return (
     <View style={styles.container}>
       <View style={styles.textContainer}>
         <Text style={styles.title}>그룹</Text>
-        <FontAwesome name="toggle-on" size={30} color="pink" />
+        <Switch
+          value={isGroupEnabled}
+          onValueChange={handleGroupToggle}
+          trackColor={{ false: colors.TEXT_GRAY, true: colors.PINK }}
+        />
       </View>
+
+      {isGroupEnabled && (
+        <View style={styles.content}>
+          <TextInput
+            style={styles.inputContainer}
+            placeholder="그룹을 찾아보세요"
+            value={searchText}
+            onChangeText={setSearchText}
+            editable={false}
+          />
+          <TouchableOpacity
+            style={styles.searchButton}
+            onPress={handleSearchPress}
+          >
+            <Text style={{ color: colors.BLACK }}>찾아보기</Text>
+          </TouchableOpacity>
+        </View>
+      )}
     </View>
   );
 };
@@ -28,5 +70,30 @@ const styles = StyleSheet.create({
     fontSize: 18,
     fontWeight: "600",
     paddingBottom: 8,
+  },
+  content: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    marginVertical: 5,
+  },
+  inputContainer: {
+    height: 40,
+    borderWidth: 0.5,
+    borderRadius: 8,
+    borderColor: colors.TEXT_GRAY,
+    paddingHorizontal: 10,
+    color: colors.TEXT_GRAY,
+    fontWeight: "500",
+    width: "78%",
+    fontSize: 16,
+  },
+  searchButton: {
+    height: 40,
+    borderWidth: 0.5,
+    borderRadius: 8,
+    borderColor: colors.TEXT_GRAY,
+    paddingHorizontal: 10,
+    justifyContent: "center",
   },
 });
